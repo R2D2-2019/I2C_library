83ea519df33564ac79a2db32364fe8eaf8233194
Florian Humblot <florian.humblot@student.hu.nl>
2019-03-20 16:38:17 +0100

    Initialise the repo

---
+++ code/main.cpp
@@ -0,0 +1,12 @@
+#include "hwlib.hpp"
+
+int main(void) {
+  // kill the watchdog
+    WDT->WDT_MR = WDT_MR_WDDIS;
+    hwlib::wait_ms(1000);
+    for (;;){
+        hwlib::cout << "this works via arduino";  
+        hwlib::wait_ms(1000);
+    }
+}
+
